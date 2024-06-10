b848708fe4d1c51a6140aa9afb0a75c1fdca24cb
maltemosbach <maltemosbach@gmail.com>
2024-06-10 11:15:59 +0200

    Enable more fine-grained control for convex decomposition.

--- ycb_converter/convex_decompose.py
+++ ycb_converter/convex_decompose.py
@@ -9,11 +9,12 @@ import trimesh
 from typing import List, Optional
 
 
-def to_convex_colliders(mesh: trimesh.Trimesh, threshold: float = 0.1, face_count: Optional[int] = 1000) -> List[trimesh.Trimesh]:
+def to_convex_colliders(mesh: trimesh.Trimesh, threshold: float = 0.07, max_convex_hull: int = -1, face_density: Optional[int] = None, 
+        min_faces: int = 12, max_faces: int = 100, preprocess_resolution: int = 10) -> List[trimesh.Trimesh]:
     coacd_mesh = coacd.Mesh(mesh.vertices, mesh.faces)
-    convex_meshes = [trimesh.Trimesh(vs, fs) for vs, fs in coacd.run_coacd(coacd_mesh, threshold=threshold)]
-    if face_count is not None:
-        return [convex_mesh.simplify_quadric_decimation(face_count) for convex_mesh in convex_meshes]
+    convex_meshes = [trimesh.Trimesh(vs, fs) for vs, fs in coacd.run_coacd(coacd_mesh, threshold=threshold, max_convex_hull=max_convex_hull, preprocess_resolution=preprocess_resolution)]
+    if face_density is not None:
+        return [convex_mesh.simplify_quadric_decimation(max(min(int(face_density * convex_mesh.area), max_faces), min_faces)) for convex_mesh in convex_meshes]
     return convex_meshes
 
 
@@ -47,10 +48,10 @@ def infer_inertial_properties(object_name: str, convex_colliders: List[trimesh.T
         json.dump(inertial_properties, f, indent=4)
 
 
-def convex_decompose_mesh(object_name: str, resolution: str = "16k", threshold: float = 0.1, face_count: Optional[int] = None) -> None:
+def convex_decompose_mesh(object_name: str, resolution: str = "16k", threshold: float = 0.07, max_convex_hull: int = -1, face_density: Optional[int] = None) -> None:
     # Infer convex decomposition from mesh.
     mesh = trimesh.load(get_mesh_directory(object_name, resolution) / "centered.obj")
-    convex_meshes = to_convex_colliders(mesh, threshold=threshold, face_count=face_count)
+    convex_meshes = to_convex_colliders(mesh, threshold=threshold, max_convex_hull=max_convex_hull, face_density=face_density)
 
     # Infer and save inertial properties of decomposed mesh.
     infer_inertial_properties(object_name, convex_meshes, resolution)
@@ -59,10 +60,10 @@ def convex_decompose_mesh(object_name: str, resolution: str = "16k", threshold: 
         convex_mesh.export(get_mesh_directory(object_name, resolution) / f"convex_collider_{i}.obj")
 
 
-def convex_decompose_meshes(resolution: str = "16k", threshold: float = 0.1, face_count: Optional[int] = None) -> None:
+def convex_decompose_meshes(resolution: str = "16k", threshold: float = 0.07, max_convex_hull: int = -1, face_density: Optional[int] = None) -> None:
     for object_name in tqdm(YCB_OBJECT_NAMES, desc="Converting meshes to convex colliders"):
         if pathlib.Path.exists(get_mesh_directory(object_name, resolution) / "centered.obj"):
-            convex_decompose_mesh(object_name, resolution=resolution, threshold=threshold, face_count=face_count)
+            convex_decompose_mesh(object_name, resolution=resolution, threshold=threshold, max_convex_hull=max_convex_hull, face_density=face_density)
 
 
 if __name__ == "__main__":
@@ -70,7 +71,8 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description="Decompose meshes into convex colliders and infer inertial properties.")
     parser.add_argument("--resolution", type=str, default="16k", help="Resolution of meshes to decompose.")
-    parser.add_argument("--threshold", type=float, default=0.1, help="Threshold for convex decomposition.")
-    parser.add_argument("--face_count", type=int, default=None, help="Target face count for each convex collision mesh.")
+    parser.add_argument("--threshold", type=float, default=0.07, help="Threshold for convex decomposition.")
+    parser.add_argument("--max-convex-hull", type=int, default=16, help="Maximum number of convex hulls in the resulting decomposition.")
+    parser.add_argument("--face-density", type=int, default=7500, help="Target face count / area for each convex collision mesh.")
     args = parser.parse_args()
-    convex_decompose_meshes(resolution=args.resolution, threshold=args.threshold, face_count=args.face_count)
+    convex_decompose_meshes(resolution=args.resolution, threshold=args.threshold, max_convex_hull=args.max_convex_hull, face_density=args.face_density)
--- ycb_converter/main.py
+++ ycb_converter/main.py
@@ -7,10 +7,10 @@ from ycb_converter.assemble_urdf import assemble_meshes_to_urdfs
 from ycb_converter.convert_to_usd import convert_urdfs_to_usds
 
 
-def main(orbit_path: Optional[str | PathLike] = None, resolution: str = "16k", threshold: float = 0.05, face_count: int = 100) -> None:
+def main(orbit_path: Optional[str | PathLike] = None, resolution: str = "16k", threshold: float = 0.07, max_convex_hull: int = -1, face_density: int = 7500) -> None:
     download_ycb_models(resolution=resolution)
     align_meshes_to_origin(resolution=resolution)
-    convex_decompose_meshes(resolution=resolution, threshold=threshold, face_count=face_count)
+    convex_decompose_meshes(resolution=resolution, threshold=threshold, max_convex_hull=max_convex_hull, face_density=face_density)
     assemble_meshes_to_urdfs(resolution=resolution)
 
     if orbit_path is not None:
@@ -23,7 +23,9 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Convert YCB models to sim-ready assets.")
     parser.add_argument("--orbit-path", type=str, default=None, help="Path to the Orbit binary.")
     parser.add_argument("--resolution", type=str, default="16k", help="Resolution of meshes to align.")
-    parser.add_argument("--threshold", type=float, default=0.05, help="Threshold for convex decomposition.")
-    parser.add_argument("--face-count", type=int, default=100, help="Face count for convex decomposition.")
+    parser.add_argument("--threshold", type=float, default=0.07, help="Threshold for convex decomposition.")
+    parser.add_argument("--max-convex-hull", type=int, default=16, help="Maximum number of convex hulls in the resulting decomposition.")
+    parser.add_argument("--face-density", type=int, default=7500, help="Target face count / area for each convex collision mesh.")
+
     args = parser.parse_args()
-    main(orbit_path=args.orbit_path, resolution=args.resolution, threshold=args.threshold, face_count=args.face_count)
+    main(orbit_path=args.orbit_path, resolution=args.resolution, threshold=args.threshold, max_convex_hull=args.max_convex_hull, face_density=args.face_density)
